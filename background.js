5c6bf403b510bd03e5eb1ebfa7cc357468750bf6
Eui Sun Ahn <joel.ahn@alumni.ubc.ca>
2020-01-12 10:14:46 -0800

    Stores as dictionary

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright 2018 TJZ. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,25 +7,25 @@
 var local = (function(){
     var setData = function(key,obj){
         var values = JSON.stringify(obj);
-        localStorage.setItem(key,values);
+        window.localStorage.setItem(key,values);
     }
 
     var getData = function(key){
-        if(localStorage.getItem(key) != null){
-            return JSON.parse(localStorage.getItem(key));
+        if(window.localStorage.getItem(key) != null){
+            return JSON.parse(window.localStorage.getItem(key));
         }else{
             return false;
         }
     }
 
     var updateData = function(key, newData){
-        if(localStorage.getItem(key) != null){
-            var oldData = JSON.parse(localStorage.getItem(key));
+        if(window.localStorage.getItem(key) != null){
+            var oldData = JSON.parse(window.localStorage.getItem(key));
             for(var keyObj in newData){
                 oldData[keyObj] = newData[keyObj];
             }
             var values = JSON.stringify(oldData);
-            localStorage.setItem(key,values);
+            window.localStorage.setItem(key,values);
         } else{
             return false;
         }
@@ -34,17 +34,6 @@ var local = (function(){
     return {set:setData,get:getData,update:updateData}
 })();
 
-var showAll = function(){
-    for (var i = 0; i < localStorage.length; i++)
-        localStorage.getItem(localStorage.key(i));
-};
-
-// var cur_tab_id;
-// chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-//     if (tabs[0])
-//         cur_tab_id = tabs[0].id;
-// });
-//
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
     if (details.method === "POST" /*&& details.tabId === cur_tab_id*/) {
         console.log("Filtering");
@@ -60,48 +49,46 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
         console.log(details.requestBody.formData);
         let post_data = details.requestBody.formData;
 
-        let key, info = [], empty = JSON.stringify([""]);
-        let keys = [];
+        let info = {email : false, address : false, payment : false, phone : false};
+        let key, keys = [];
+        const empty = JSON.stringify([""]);
         for (key in post_data) {
             let value = JSON.stringify(post_data[key]);
             if (/email/.test(key) && !/use/.test(key) && value != empty) {
                 console.log(key);
+                info.email = true;
                 keys.push(key);
-                if (info.indexOf("email") === -1)
-                    info.push("email");
             }
             if(/address/.test(key) && value != empty) {
                 console.log(key);
+                info.address = true;
                 keys.push(key);
-                if (info.indexOf("address") === -1)
-                    info.push("address");
             }
             if(/cc.*number/.test(key) && value != empty) {
                 console.log(key);
+                info.payment = true;
                 keys.push(key);
-                if (info.indexOf("payment") === -1)
-                info.push("payment");
             }
             if(/phone/.test(key) && value != empty) {
                 console.log(key);
+                info.phone = true;
                 keys.push(key);
-                if (info.indexOf("phone") === -1)
-                    info.push("phone");
             }
         }
 
         // Grab the fields being sent to the person via the request
-        if (info.length !== 0) {
-            console.log("Info being given: " + info);
+        if (keys.length !== 0) {
             var give = window.confirm("Are you sure you want to give this info?");
             if (give) {
                 local.set(initiator, info);
-                // chrome.storage.local.set(dataObj, function() {
-                //     console.log("Saved " + initiator);
-                // });
+                let data = local.get(initiator);
+                console.log(data.email);
+                console.log(data.address);
+                console.log(data.payment);
+                console.log(data.phone);
             }
+            console.log("Cancelling POST request: " + !give);
             return {cancel : !give};
-            console.log("Cancelling POST request: " + give);
         }
     }
 
